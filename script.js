4d5cd984f1e2301fc26e3a00dbadb40e6ce434e8
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2024-03-28 19:34:18 +0100

    I dont like it, but it kinda wordks.

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ window.addEventListener('resize', () => {
 canvas.addEventListener('click', (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
-  // for(let i = 0; i < 10; i++) {
-  //   particlesArray.push(new Particle());
-  // }
+  for(let i = 0; i < 1; i++) {
+    particlesArray.push(new Particle());
+  }
 });
 canvas.addEventListener('mousemove', (event) => {
   mouse.x = event.x;
@@ -31,12 +31,13 @@ canvas.addEventListener('mousemove', (event) => {
 
 class Particle {
   constructor() {
-    this.size = Math.random() * 17;
+    this.size = 40;
     this.x = (Math.random() * (canvas.width - (this.size + 20))) + this.size;
     this.y = (Math.random() * (canvas.height - (this.size + 20))) + this.size;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 -1.5;
-    this.color = `hsl(${hue}, 100%, 50%)`
+    this.color = `hsla(${hue}, 100%, 50%, 1)`;
+    this.background = `hsla(${hue}, 50%, 20%, 1)`
   }
   update() {
     if(this.x <= 0 + this.size 
@@ -50,15 +51,35 @@ class Particle {
     this.x += this.speedX;
     this.y += this.speedY;
   }
+  reverseSX() {
+    this.speedX = this.speedX * - 1;
+  }
+  reverseSY() {
+    this.speedY = this.speedY * - 1;
+  }
   draw() {
-    ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.strokeStyle = this.color;
+    ctx.stroke();
+
+    const gradient = ctx.createRadialGradient(
+      this.x,
+      this.y,
+      0,
+      this.x - 10,
+      this.y - 10,
+      this.size - 10
+    );
+    gradient.addColorStop(0.25, "white");
+    gradient.addColorStop(1, this.color);
+    ctx.fillStyle = gradient;
     ctx.fill();
   }
 }
 function init() {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 20; i++) {
+    hue += 6;
     particlesArray.push(new Particle());
   }
 }
@@ -70,17 +91,16 @@ function handleParticles() {
       const dx = particlesArray[i].x - particlesArray[j].x;
       const dy = particlesArray[i].y - particlesArray[j].y;
       const distance = Math.sqrt(dx * dx + dy * dy);
-      if(distance < 100) {
-        ctx.beginPath();
-        ctx.strokeStyle = particlesArray[i].color;
-        // ctx.lineWidth = particlesArray[i].size / 10;
-        ctx.lineWidth = 0.5;
-        ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
-        ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
-        ctx.stroke();
-        ctx.closePath();
-
-      }
+      // if(distance < 100) {
+      //   ctx.beginPath();
+      //   ctx.strokeStyle = particlesArray[i].color;
+      //   ctx.lineWidth = particlesArray[i].size / 10;
+      //   ctx.lineWidth = 0.5;
+      //   ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+      //   ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+      //   ctx.stroke();
+      //   ctx.closePath();
+      // }
     }
     if(particlesArray[i].size <= 0.3){
       particlesArray.splice(i, 1);
