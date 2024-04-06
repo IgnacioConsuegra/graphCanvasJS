105a2011e890c436b0fd340400bf501e637d3bda
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2024-04-06 20:13:05 +0200

    basic align finished.

--- script.js
+++ script.js
@@ -133,56 +133,119 @@ function handleParticlesArrPosition(node){
   }
 }
 function alignNodesPosition(){
-  try{
-    for(let i = particlesLeftAndRightPositions.length - 1; i > 0; i--) { 
-      particlesLeftAndRightPositions[i].map((parHolder, parHolderPosition) => {
-        if(parHolderPosition === 0 ) {
-            const thisArrLength = particlesLeftAndRightPositions[i][parHolderPosition].length;
-            if(thisArrLength === 0 ) {
-              return;
-            }
-            const rightest = particlesLeftAndRightPositions[i][parHolderPosition][thisArrLength - 1];
-            const rightestPosition = rightest.x; 
-            if(rightestPosition > RIGHTEST_lEFT_POSSIBLE) { 
-              particlesLeftAndRightPositions[i][parHolderPosition][thisArrLength - 1].x = RIGHTEST_lEFT_POSSIBLE;
-              let last_Particle_Position = particlesLeftAndRightPositions[i][parHolderPosition][thisArrLength - 1].x;
-              console.log("rightest : ", last_Particle_Position);
-              for(let h = particlesLeftAndRightPositions[i][parHolderPosition].length - 2; h >= 0; h--) {
-                particlesLeftAndRightPositions[i][parHolderPosition][h].x = last_Particle_Position - NODES_WIDTH_DISTANCE; 
-                last_Particle_Position = particlesLeftAndRightPositions[i][parHolderPosition][h].x;
-              }
-            }
-        }
-        if(parHolderPosition === 1 ) {
-          const thisArrLength = particlesLeftAndRightPositions[i][parHolderPosition].length;
-          if(thisArrLength === 0 ) {
-            return;
+  function handleXOverflow() {
+    const particlesLRLength = particlesLeftAndRightPositions.length;
+    for(let row = particlesLRLength - 1; row > 0; row--) {
+      const leftArrLength = particlesLeftAndRightPositions[row][0].length;
+      const rightArrLength = particlesLeftAndRightPositions[row][1].length;
+
+      
+      let rightLeftNode = -Infinity;
+      let leftestRightNode = Infinity;
+      if(leftArrLength){rightLeftNode = particlesLeftAndRightPositions[row][0][leftArrLength - 1].x}
+      if(rightArrLength){leftestRightNode = particlesLeftAndRightPositions[row][1][0].x}
+      if(rightLeftNode >= RIGHTEST_lEFT_POSSIBLE) {
+        //Let's align rightest particle, and let's align all row. 
+        particlesLeftAndRightPositions[row][0][leftArrLength - 1].x = RIGHTEST_lEFT_POSSIBLE;
+        let lastParticle = particlesLeftAndRightPositions[row][0][leftArrLength - 1].x;
+        for(let particle = leftArrLength - 2; particle >= 0; particle--) {
+          const particlePosition = particlesLeftAndRightPositions[row][0][particle].x;
+          if(lastParticle - particlePosition < 30){
+            particlesLeftAndRightPositions[row][0][particle].x = lastParticle - NODES_WIDTH_DISTANCE;
           }
-          const lefTest = particlesLeftAndRightPositions[i][parHolderPosition][0];
-          const lefTestPosition = lefTest.x; 
-          if(lefTestPosition < LEFTEST_RIGHT_POSSIBLE) { 
-            particlesLeftAndRightPositions[i][parHolderPosition][0].x = LEFTEST_RIGHT_POSSIBLE;
-            let last_Particle_Position = particlesLeftAndRightPositions[i][parHolderPosition][0].x;
-              
-            for(let h = 1; h >= thisArrLength - 1; h--) {
-              particlesLeftAndRightPositions[i][parHolderPosition][h].x = last_Particle_Position + NODES_WIDTH_DISTANCE; 
-              last_Particle_Position = particlesLeftAndRightPositions[i][parHolderPosition][h].x;
-            }
-        } 
+          lastParticle = particlesLeftAndRightPositions[row][0][particle].x;
         }
-      })
+        //Let's align uppers rows.
+        for(let rowsA = row - 1; rowsA > 0; rowsA--) {
+          const thisArrLength = particlesLeftAndRightPositions[rowsA][0].length;
+          for(let particle = 0; particle < thisArrLength; particle++){
+            const thisParticle = particlesLeftAndRightPositions[rowsA][0][particle];
+            if(thisParticle.left) {
+              thisParticle.x = thisParticle.left.x + NODES_WIDTH_DISTANCE  / 2;
+              continue;
+            }
+            if(thisParticle.right) {
+              console.log("Particle right : ", thisParticle.right)
+              thisParticle.x = thisParticle.right.x - NODES_WIDTH_DISTANCE / 2;
+              continue;
+            }
+          }
+        }
+      }
+      if(leftestRightNode <= LEFTEST_RIGHT_POSSIBLE) {
+        //Let's align rightest particle, and let's align all row. 
+        particlesLeftAndRightPositions[row][1][0].x = LEFTEST_RIGHT_POSSIBLE;
+        let lastParticle = particlesLeftAndRightPositions[row][1][0].x;
 
+        for(let particle = 1; particle <= rightArrLength - 1; particle++) {
+          const particlePosition = particlesLeftAndRightPositions[row][1][particle].x;
+          if(particlePosition - lastParticle  < 30){
+            particlesLeftAndRightPositions[row][1][particle].x = lastParticle + NODES_WIDTH_DISTANCE;
+          }
+          lastParticle = particlesLeftAndRightPositions[row][1][particle].x;
+        }
+        //Let's align uppers rows.
+        for(let rowsA = row - 1; rowsA > 1; rowsA--) {
+          const thisArrLength = particlesLeftAndRightPositions[rowsA][1].length;
+
+          for(let particle = 1; particle < thisArrLength; particle++){
+            const thisParticle = particlesLeftAndRightPositions[rowsA][1][particle];
+            if(thisParticle.left) {
+              thisParticle.x = thisParticle.left.x - NODES_WIDTH_DISTANCE  / 2;
+              continue;
+            }
+            if(thisParticle.right) {
+              thisParticle.x = thisParticle.right.x + NODES_WIDTH_DISTANCE / 2;
+              continue;
+            }
+          }
+        }
+      }
     }
-  }catch(err) {
+  }
+  function respectSpaces() {
+    const particlesLRLength = particlesLeftAndRightPositions.length;
+    for(let row = particlesLRLength - 1; row > 0; row--) {
+      const leftArrLength = particlesLeftAndRightPositions[row][0].length;
+      const rightArrLength = particlesLeftAndRightPositions[row][1].length;
 
+      if(leftArrLength > 1) {
+        let lastParticle = particlesLeftAndRightPositions[row][0][leftArrLength - 1].x;
+        for(let particle = leftArrLength - 2; particle >= 0; particle--) {
+          const particlePosition = particlesLeftAndRightPositions[row][0][particle].x;
+          if(lastParticle - particlePosition < 30){
+            particlesLeftAndRightPositions[row][0][particle].x = lastParticle - NODES_WIDTH_DISTANCE;
+          }
+          lastParticle = particlesLeftAndRightPositions[row][0][particle].x;
+        }
+      } 
+
+      if(rightArrLength > 1) {
+        let lastParticle = particlesLeftAndRightPositions[row][1][0].x;
+        for(let particle = 1; particle <= rightArrLength - 1; particle++) {
+          const particlePosition = particlesLeftAndRightPositions[row][1][particle].x;
+          if(particlePosition - lastParticle < 30){
+            particlesLeftAndRightPositions[row][1][particle].x = lastParticle + NODES_WIDTH_DISTANCE;
+          }
+          lastParticle = particlesLeftAndRightPositions[row][1][particle].x;
+        }
+      } 
+    }
+  }
+  try{
+    handleXOverflow();
+    respectSpaces();
+
+  }catch(err) {
+    console.log(err)
   }
 }
 function init() {
-  for(let i = 0; i < 9; i++ ) {
+  for(let i = 0; i < 26; i++ ) {
     if(i === 0) {
       const node = new Particle(middleX + 0, 100, NODES_SIZE);
       particlesArray.push(node);
-      particlesLeftAndRightPositions.push([node.x]);
+      particlesLeftAndRightPositions.push([node]);
       myTree.add(node);
       continue;
     }
@@ -255,9 +318,9 @@ function init() {
       myTree.add(left);
     }
   }
-  alignNodesPosition();
   console.log(myTree);
   console.log(particlesLeftAndRightPositions);
+  alignNodesPosition();
 }
 
 function handleParticles() {
@@ -316,6 +379,7 @@ function animate() {
 }
 init();
 handleParticles();
+
 // animate();
 
 
